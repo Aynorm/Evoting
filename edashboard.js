690f3a75ceab72df9824a91996bc95b7fb25b5d5
Aynorm <116764902+Aynorm@users.noreply.github.com>
2025-11-15 06:30:58 -0800

    Add files via upload

--- edashboard.js
+++ edashboard.js
@@ -1,72 +1,91 @@
 // script.js
 
-      // Initialize vote counts
-      let votes = {
-        "Option 1": 0,
-        "Option 2": 0,
-        "Option 3": 0,
-        "Option 4": 0,
-        "Option 5": 0,
+// Initialize vote counts
+let votes = {
+  "Option 1": 0,
+  "Option 2": 0,
+  "Option 3": 0,
+  "Option 4": 0,
+  "Option 5": 0,
+};
 
-      };
-
-      // Function to handle voting
-      function vote(option) {
-        votes[option]++;
-        updateResults();
-      }
-
-      // Function to update the results display
-      function updateResults() {
-        document.getElementById("option1-count").innerText = votes["Option 1"];
-        document.getElementById("option2-count").innerText = votes["Option 2"];
-        document.getElementById("option3-count").innerText = votes["Option 3"];
-        document.getElementById("option4-count").innerText = votes["Option 4"];
-        document.getElementById("option5-count").innerText = votes["Option 5"];
-      }
-
-//Result overlay hide  
-      function openNav() {
-        document.getElementById("myNav").style.width = "100%";
-      }
-      function closeNav() {
-        document.getElementById("myNav").style.width = "0%";
+// Function to handle voting
+function vote(option) {
+  votes[option]++;
+  updateResults();
 }
-      
+
+// Function to update the results display
+function updateResults() {
+  document.getElementById("option1-count").innerText = votes["Option 1"];
+  document.getElementById("option2-count").innerText = votes["Option 2"];
+  document.getElementById("option3-count").innerText = votes["Option 3"];
+  document.getElementById("option4-count").innerText = votes["Option 4"];
+  document.getElementById("option5-count").innerText = votes["Option 5"];
+}
+
+//Result overlay hide
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
 // For my Vote
-      function openVote() {
-        document.getElementById("myVote").style.width = "100%";
-      }
-      function closeVote() {
-        document.getElementById("myVote").style.width = "0%";
+function openVote() {
+  document.getElementById("myVote").style.width = "100%";
+}
+function closeVote() {
+  document.getElementById("myVote").style.width = "0%";
 }
 
-      function openVote1() {
-        document.getElementById("myVote1").style.width = "100%";
-      }
-      function closeVote1() {
-        document.getElementById("myVote1").style.width = "0%";
+function openVote1() {
+  document.getElementById("myVote1").style.width = "100%";
+}
+function closeVote1() {
+  document.getElementById("myVote1").style.width = "0%";
 }
 
-      function openVote2() {
-        document.getElementById("myVote2").style.width = "100%";
-      }
-      function closeVote2() {
-        document.getElementById("myVote2").style.width = "0%";
+function openVote2() {
+  document.getElementById("myVote2").style.width = "100%";
+}
+function closeVote2() {
+  document.getElementById("myVote2").style.width = "0%";
 }
 
-      function openVote3() {
-        document.getElementById("myVote3").style.width = "100%";
-      }
-      function closeVote3() {
-        document.getElementById("myVote3").style.width = "0%";
+function openVote3() {
+  document.getElementById("myVote3").style.width = "100%";
+}
+function closeVote3() {
+  document.getElementById("myVote3").style.width = "0%";
 }
 
-      function openVote4() {
-        document.getElementById("myVote4").style.width = "100%";
-      }
-      function closeVote4() {
-        document.getElementById("myVote4").style.width = "0%";
+function openVote4() {
+  document.getElementById("myVote4").style.width = "100%";
+}
+function closeVote4() {
+  document.getElementById("myVote4").style.width = "0%";
 }
 
-      
+//Make Payment Overlay JS
+const makePaymentOverlay = document.getElementById("paymentOverlay");
+const paymentOpenBtn = document.getElementById("paymentOpenBtn");
+const paymentCloseBtn = document.getElementById("paymentCloseBtn");
+
+//Show Payment open btn
+paymentOpenBtn.addEventListener("click", () => {
+  makePaymentOverlay.style.display = "flex";
+});
+
+//Close Payment btn
+paymentCloseBtn.addEventListener("click", () => {
+  makePaymentOverlay.style.display = "none";
+});
+
+//Close Overlay when user click outside content
+makePaymentOverlay.addEventListener("click", (e) => {
+  if (e.target === makePaymentOverlay) {
+    makePaymentOverlay.style.display = "none";
+  }
+});
--- enavbarstandby.js
+++ enavbarstandby.js
@@ -87,3 +87,5 @@ export const navBottom =
   </ul >\
   </div >\
 ";
+
+//Theme switch
